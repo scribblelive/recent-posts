a6cffb39971e5cfd955790b99b107f1a17ded0fc
Jasna Todorovic <jasna.todorovic@scribblelive.com>
2013-10-17 09:23:44 -0400

    Revert "Revert "SCRIB-1016 SCRIB-8213 SCRIB-8214 SCRIB-8215 SCRIB-8216""
    This reverts commit a8fe72aee1a7e6b4025a4c3e785bd71655a6d3fc.

--- js/SLRecentPostsv1.js
+++ js/SLRecentPostsv1.js
@@ -65,51 +65,26 @@ function RecentPosts(Options) {
     // Set an index to keep track of multiple events using this script.
     this.InstanceIndex = RecentPosts.StoreInstance(this);
 
-	// Call the function that creates the element that the posts will be added to.
-    this.CreatePostList();
+    var _self = this,
+        S;
 
-    // Call the function that loads the most recent posts.
-    this.GetAllPosts();
+    var interval = setInterval( function() {
 
-    var _self = this;
-    // Attach event handlers for previous/next links for slideshows
-    $(".SlideShowNext").live('click', function (e) {
-        e.preventDefault();
-        var SlideShowDiv = $(this).parent();
-        var VisibleSlide = SlideShowDiv.find(".SL_SlideShow_Slide").not(".HideSlide");
-        var NextSlide = VisibleSlide.nextAll(".HideSlide").first();
+        if( typeof Scrbbl !== 'undefined' ) {
 
-        if (NextSlide.length === 0) {
-            NextSlide = SlideShowDiv.find(".SL_SlideShow_Slide:first");
-            _self.UpdateSlideCurrent(SlideShowDiv, 1);
+            RecentPosts.__S = new Scrbbl(_self.Options.EventId);
+            // Call the function that creates the element that the posts will be added to.
+            _self.CreatePostList();
+
+            // Call the function that loads the most recent posts.
+            _self.GetAllPosts();
+
+            clearInterval( interval );
         }
-        else {
-            _self.UpdateSlideCurrent(SlideShowDiv, parseInt(SlideShowDiv.find(".SL_SlideShow_Current").html(), 10) + 1);
-        }
-        NextSlide.removeClass("HideSlide");
-        VisibleSlide.addClass("HideSlide");
-        return false;
-    });
 
-    $(".SlideShowPrev").live('click', function (e) {
-        e.preventDefault();
-        var SlideShowDiv = $(this).parent();
-        var VisibleSlide = SlideShowDiv.find(".SL_SlideShow_Slide").not(".HideSlide");
-        var PrevSlide = VisibleSlide.prevAll(".HideSlide").first();
+    }, 200 );
 
-        if (PrevSlide.length === 0) {
-            PrevSlide = SlideShowDiv.find(".SL_SlideShow_Slide:last");
-            _self.UpdateSlideCurrent(SlideShowDiv, SlideShowDiv.find(".SL_SlideShow_Slide").length);
-        }
-        else {
-            _self.UpdateSlideCurrent(SlideShowDiv, parseInt(SlideShowDiv.find(".SL_SlideShow_Current").html(), 10) - 1);
-        }
-        PrevSlide.removeClass("HideSlide");
-        VisibleSlide.addClass("HideSlide");
-        return false;
-    });
-
-
+    $('body').append('<script type="text/javascript" src="js/Scrbbl.js"></script>');
 
     (function( w, d, eid ){
           var id = 'sl-libjs', where = d.getElementsByTagName( 'script' )[0];
@@ -156,7 +131,6 @@ RecentPosts.StoreInstance = function (pRecentPostsInstance) {
 
 // The function that decides what to do with the response it gets back from the api.
 RecentPosts.prototype.DrawPosts = function (pResponse) {
-
 	var CurrentPostsList = this.GetPostList();
 
     // Add any new posts.
@@ -209,7 +183,6 @@ RecentPosts.prototype.DrawPosts = function (pResponse) {
 
 // The function that adds a post.
 RecentPosts.prototype.AddPost = function (pPost, pPostList) {
-
     // A huge if statement that decides if it should be showing a post or not based on the options set when the widget is loaded.
     if (
         (pPost.Type === "IMAGE" && !this.Options.ShowImages) ||
@@ -254,12 +227,12 @@ RecentPosts.prototype.AddPost = function (pPost, pPostList) {
 
     // Add any image, video, or audio to the post content div.
     if (pPost.Media !== undefined) {
-        NewContentDiv.innerHTML = this.AddMedia(pPost);
+        NewContentDiv.innerHTML = RecentPosts.__S.AddMedia(pPost);
     }
 
     // If the post is a poll, display the poll.
     else if (pPost.Type === "POLL") {
-        NewContentDiv.appendChild(this.DisplayPoll(pPost.Entities, false));
+        NewContentDiv.appendChild(RecentPosts.__S.DisplayPoll(pPost.Entities, false));
     }
 
     // Add the regular content if the post is not media or a poll.
@@ -311,17 +284,14 @@ RecentPosts.prototype.AddPost = function (pPost, pPostList) {
         document.getElementById("RecentPostsWidget" + this.InstanceIndex).insertBefore(NewListItem, document.getElementById("RecentPostsWidget" + this.InstanceIndex).firstChild);
     }
 
-    var _self = this;
-    setTimeout(function () {
-        _self.ResizeAllSlideshows(NewListItem);
-    }, 200);
+    if(jQuerySL(NewListItem).find(".SL_SlideShow").length){
+        setTimeout(function () {
+            RecentPosts.__S.ResizeAllSlideshows(NewListItem);
+        }, 200);
+    }
 
 };
 
-RecentPosts.prototype.UpdateSlideCurrent = function (pSlideShow, pValue) {
-    $(pSlideShow).find(".SL_SlideShow_Current").html(pValue);
-    $(".FloatingXButton,.FloatingEditButton").remove();
-};
 
 // The function that deletes a post.
 RecentPosts.prototype.DeletePost = function (pPostId) {
@@ -346,234 +316,6 @@ RecentPosts.prototype.EditPost = function (pPostToEdit) {
             }
         }
     }
-};
-
-// The function that adds images, video, and audio to posts containing media that are added or edited.
-RecentPosts.prototype.AddMedia = function (pPost) {
-    var Media = pPost.Media;
-	var MediaHtml;
-	for (var m = 0; m < Media.length; m++) {
-		if (pPost.Type === "IMAGE" && Media[m].Type === "IMAGE") {
-			MediaHtml = "<img src='" + Media[m].Url + "'/>";
-		}
-		if (pPost.Type === "VIDEO" && Media[m].Type === "VIDEO") {
-			MediaHtml = "<embed type='application/x-shockwave-flash' src='http://embed.scribblelive.com/js/jwflvplayer/player-licensed.swf?ThreadId=" + this.Options.EventId + "' flashvars='file=" + Media[m].Url + "'>";
-		}
-		if (pPost.Type === "AUDIO" && Media[m].Type === "AUDIO") {
-			MediaHtml = "<embed height='20' width='300' type='application/x-shockwave-flash' src='http://embed.scribblelive.com/js/jwflvplayer/player-licensed.swf?ThreadId=" + this.Options.EventId + "' flashvars='file=" + Media[m].Url + "'>";
-		}
-	}
-
-    // Add the caption to the media added above.
-    var NewContent;
-    if ((pPost.Content !== "") && (pPost.Content !== undefined) && (this.Options.ShowCaptions)) {
-        var MediaCaption = "<p class='Caption'>" + pPost.Content + "</p>";
-        NewContent = MediaHtml + MediaCaption;
-    } else {
-        NewContent = MediaHtml;
-    }
-
-    return NewContent;
-};
-
-// The function that displays a poll both before and after voting.
-RecentPosts.prototype.DisplayPoll = function (pPostContent, AlreadyVoted) {
-
-    var ThePoll = document.createElement("dl");
-    ThePoll.id = "Poll" + pPostContent.Id;
-
-    var TheQuestion = document.createElement("dt");
-    TheQuestion.innerHTML = pPostContent.Question;
-    ThePoll.appendChild(TheQuestion);
-
-    var PollAnswers = pPostContent.Answers;
-    for (var p = 0; p < PollAnswers.length; p++) {
-        var PollQuestion = document.createElement("dd");
-        PollQuestion.id = "Answer" + PollAnswers[p].Id;
-        if (AlreadyVoted === false) {
-			PollQuestion.innerHTML = "<a class='Vote' onclick='RecentPosts.GetInstance(" + this.InstanceIndex + ").VoteOnPoll(" + pPostContent.Id + "," + PollAnswers[p].Id + ")'><input type='radio' /></a><span>" + PollAnswers[p].Text + "</span>";
-		} else if (AlreadyVoted === true) {
-			PollQuestion.innerHTML = PollAnswers[p].Text + " (" + PollAnswers[p].Votes + " votes)";
-		}
-        ThePoll.appendChild(PollQuestion);
-    }
-
-    return ThePoll;
-};
-
-// The function that is called when a user votes on a poll.
-RecentPosts.prototype.VoteOnPoll = function (pPollId, pAnswerId) {
-	// Figure out if the user has already voted by setting a class and checking for that class before submitting the vote.
-	var AlreadyVoted = false;
-	var ChosenAnswer = document.getElementById("Answer" + pAnswerId);
-	for (var v = 0; v < ChosenAnswer.parentNode.childNodes.length; v++) {
-		if (ChosenAnswer.parentNode.childNodes[v].className === "Voting") {
-			AlreadyVoted = true;	
-		}
-	}
-	if (AlreadyVoted === true) {
-		return;
-	}
-	ChosenAnswer.className = "Voting";
-	// Add the api call that submits the vote.
-	var VotePollAPICall = document.createElement("script");
-    VotePollAPICall.type = "text/javascript";
-    VotePollAPICall.src = "http://apiv1.scribblelive.com/poll/" + pPollId + "/vote/" + pAnswerId + "?callback=RecentPosts.GetInstance(" + this.InstanceIndex + ").PollVoteSuccess";
-    document.getElementsByTagName("head")[0].appendChild(VotePollAPICall);
-};
-
-// The function that gets the response after someone votes on a poll, and updates the poll results.
-RecentPosts.prototype.PollVoteSuccess = function (pPollResponse) {
-    // Remove the script used to submit the vote to the api.
-	var Scripts = document.getElementsByTagName("head")[0].getElementsByTagName("script");
-    for (var i = 0; i < Scripts.length; i++) {
-        var ScriptSource = Scripts[i].src;
-        if (ScriptSource.match("^\http:\/\/apiv1\.scribblelive\.com/poll/" + pPollResponse.Id + ".*") !== null) {
-            document.getElementsByTagName("head")[0].removeChild(Scripts[i]);
-        }
-    }
-	// Remove the voting elements and display the results.
-	var ThePoll = document.getElementById("Poll" + pPollResponse.Id);
-	var PollParent = ThePoll.parentNode;
-	PollParent.removeChild(ThePoll);
-	PollParent.appendChild(this.DisplayPoll(pPollResponse, true));
-};
-
-//the function that resizes a slideshow
-RecentPosts.prototype.ResizeSlideshow = function (slideshowContainer) {
-    var max = 0,
-        maxImg = 0,
-        maxImgW = 0,
-        maxCaption = 0,
-        counterHeight = 0;
-
-    if (!$(slideshowContainer).hasClass('SL_SlideShow')) {
-        slideshowContainer = $(slideshowContainer).closest('.SL_SlideShow');
-        if (!slideshowContainer.length) {
-            return;
-        }
-    }
-
-    $('.SL_SlideShow_Slide .MediaWrap', slideshowContainer).css('height', 'auto');
-    $('.SL_SlideShow_Slide .Media', slideshowContainer).css('height', 'auto');
-    $('.SL_SlideShow_Counter', slideshowContainer).css('height', 'auto');
-
-    counterHeight = slideshowContainer.find('.SL_SlideShow_Counter').height();
-
-    //use content div to determine size
-    $('.SL_SlideShow_Slide', slideshowContainer).each(function (i, el) {
-
-        var $slide = $(this),
-        IsTweet = $slide.find('.sltc-twitter').length > 0;
-        $img = IsTweet ? $slide.find('.sltc-twitter') : $slide.find('img'),
-        $media = $img.parent('.Media');
-        $meta = $slide.find('.Meta'),
-        $caption = $slide.find('.Caption');
-
-        //set tweet width to parent
-        if (IsTweet && $img.find("img").not(".sltc-avatar img").length == 0) {
-            $img.css('width', $media.width() - ($img.outerWidth() - $img.width()));
-        }
-
-        // Determine tallest container dimension
-        if ($img.outerHeight() > maxImg) {
-            maxImg = $img.outerHeight();
-        }
-
-        // Determine widest container dimension
-        if ($img.width() > maxImgW) {
-            maxImgW = $img.width();
-        }
-
-        // Determine tallest caption dimensions
-        if (($caption.outerHeight() + $meta.outerHeight()) > maxCaption) {
-            maxCaption = ($caption.outerHeight() || 0) + ($meta.outerHeight() || 0);
-        }
-
-        if (maxCaption < counterHeight) {
-            maxCaption = counterHeight;
-        }
-
-
-    });
-
-    max = maxImg + maxCaption;
-
-    // Set dimensions of containers
-    if (max > 0) {
-        $('.SL_SlideShow_Slide .MediaWrap', slideshowContainer).css('height', max);
-        $('.SL_SlideShow_Slide .Media', slideshowContainer).css('height', maxImg);
-        $('.SL_SlideShow_Slide .MetaWrap', slideshowContainer).css('top', maxImg);
-        $('.SL_SlideShow_Counter', slideshowContainer).css('height', max - maxImg);
-    }
-
-    $('.SlideShowNav', slideshowContainer).css('top', maxImg / 2);
-
-    $slideContent = $('.SL_SlideShow_Slide .sltc-twitter').add($('.SL_SlideShow_Slide img').not('.sltc-twitter img'));
-
-    // set content dimensions and center within the container
-    $($slideContent, slideshowContainer).each(function (i, el) {
-        var $img = $(el),
-            $media = $(el).parents('.Media'),
-            diff = ((maxImg - $img.height()) / 2),
-            $width = $img.outerWidth(),
-            $height = $img.outerHeight();
-
-        //special case for twitter text slides
-        if ($img.hasClass("sltc-twitter")) {
-            $img.css({
-                'position': 'absolute',
-                'top': '50%',
-                'margin-top': (($height / 2) * -1),
-                'width': $media.width() - ($img.outerWidth() - $img.width())
-            });
-        } else if ($height < maxImg && $width < $media.width()) {
-            // center image both horiz. and vert.
-            $img.css({
-                'position': 'absolute',
-                'margin-left': (($width / 2) * -1),
-                'margin-top': (($height / 2) * -1),
-                'left': '50%',
-                'top': '50%'
-            });
-        } else if ($height < maxImg) {
-            // center image vertically
-            $img.css({
-                'position': 'absolute',
-                'margin-left': '0',
-                'left': '0',
-                'margin-top': (($height / 2) * -1),
-                'top': '50%'
-            });
-        } else if ($width < $media.width()) {
-            // center image horizontally
-            $img.css({
-                'position': 'absolute',
-                'margin-top': '0',
-                'top': '0',
-                'margin-left': (($width / 2) * -1),
-                'left': '50%'
-            });
-        } else {
-            // no centering required.
-            $img.css({
-                'position': 'relative',
-                'top': '0',
-                'margin-top': '0',
-                'left': '0',
-                'margin-left': '0'
-            });
-        }
-    });
-    
-};
-
-//resize all slideshows in a container
-RecentPosts.prototype.ResizeAllSlideshows = function (container) {
-    var _self = this;
-    jQuerySL(container).find('.SL_SlideShow').each(function (i, el) {
-        _self.ResizeSlideshow(jQuerySL(el));
-    });
 };
 
 // Create a list of posts currently on the page by finding all list items inside the RecentPostsWidget list and adding their ids to an array.
@@ -612,7 +354,7 @@ RecentPosts.prototype.GetNewPosts = function (pLastPostTime) {
 RecentPosts.prototype.CreatePostList = function () {
     var PostList = document.createElement("ul");
     PostList.setAttribute("id", "RecentPostsWidget" + this.InstanceIndex);
-    PostList.className = "RecentPostsWidget";
+    PostList.className = "SL-Posts";
     document.getElementById(this.Options.WhereToAddPosts).appendChild(PostList);
 };
 
